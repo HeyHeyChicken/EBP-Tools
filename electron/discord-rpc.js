e41aa173fa16e74e3c16b3a65115eb315bd78185
aduval <aduval@assystem.com>
2025-06-18 12:13:14 +0200

    Update discord-rpc.js

--- electron/discord-rpc.js
+++ electron/discord-rpc.js
@@ -4,26 +4,26 @@
 
 //#region Imports
 
-const rpc = require('discord-rpc');
+const rpc = require("discord-rpc");
 
 //#endregion
 
-const CLIENT_ID = '1383002798882291722';
-const RPC = new rpc.Client({ transport: 'ipc' });
+const CLIENT_ID = "1383002798882291722";
+const RPC = new rpc.Client({ transport: "ipc" });
 
-RPC.on('ready', () => {
-    console.log('[DISCORD RPC] Connection successful.');
-    RPC.setActivity({
-        details: "Working on my strategies...",
-        state: 'https://ebp.gg',
-        startTimestamp: new Date(),
-        largeImageKey: 'logo',
-        largeImageText: 'EBP - Tools',
-        smallImageText: 'EBP - Tools',
-        instance: false,
-    });
+RPC.on("ready", () => {
+  console.log("[DISCORD RPC] Connection successful.");
+  RPC.setActivity({
+    details: "Working on my strategies...",
+    state: "https://ebp.gg",
+    startTimestamp: new Date(),
+    largeImageKey: "logo",
+    largeImageText: "EVA Battle Plan - Tools",
+    smallImageText: "EVA Battle Plan - Tools",
+    instance: false,
+  });
 });
 
-RPC.login({ clientId: CLIENT_ID }).catch(err => {
-    console.error('[DISCORD RPC] Connection error.');
+RPC.login({ clientId: CLIENT_ID }).catch((err) => {
+  console.error("[DISCORD RPC] Connection error.");
 });
